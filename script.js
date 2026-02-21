ba8a33b7406ded4228bf912df2ea196e6b0ac244
Pratapsmj <128983816+Pratapsmj@users.noreply.github.com>
2026-02-21 21:55:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function login(){
+  alert("Login Success (Demo)");
+  window.location.href="dashboard.html";
+}
+
+function register(){
+  alert("Registered Successfully (Demo)");
+  window.location.href="login.html";
+}
